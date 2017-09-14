fc8fff5d98ae8174e1fe6ee60ea381ed4e8eef5f
qingfeng <qingfeng@showjoy.com>
2017-09-14 22:47:50 +0800

    增加 无限循环 和 滚动 开关

--- app/src/main/java/com/fqxyi/convenientbanner/MainActivity.java
+++ app/src/main/java/com/fqxyi/convenientbanner/MainActivity.java
@@ -3,6 +3,7 @@ package com.fqxyi.convenientbanner;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.fqxyi.library.ConvenientBanner;
 import com.fqxyi.library.holder.CBViewHolderCreator;
@@ -12,6 +13,9 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     private ConvenientBanner banner;
+
+    private Button loopBtn;
+    private Button scrollBtn;
 
     private String[] images = {
             "http://img2.3lian.com/2014/f4/25/d/85.jpg",
@@ -33,6 +37,38 @@ public class MainActivity extends AppCompatActivity {
                 return new ImageHolderView();
             }
         }, Arrays.asList(images));
+
+        loopBtn = (Button) findViewById(R.id.loop_btn);
+        scrollBtn = (Button) findViewById(R.id.scroll_btn);
+
+        initEvent();
+    }
+
+    private void initEvent() {
+        loopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (banner.isCanLoop()) {
+                    banner.setCanLoop(false);
+                    loopBtn.setText("开启无限循环");
+                } else {
+                    banner.setCanLoop(true);
+                    loopBtn.setText("关闭无限循环");
+                }
+            }
+        });
+        scrollBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (banner.isCanScroll()) {
+                    banner.setCanScroll(false);
+                    scrollBtn.setText("开启滚动");
+                } else {
+                    banner.setCanScroll(true);
+                    scrollBtn.setText("关闭滚动");
+                }
+            }
+        });
     }
 
     public void pointerType1Left(View view) {
